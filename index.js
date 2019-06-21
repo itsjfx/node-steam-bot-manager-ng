dce97b7bc71fba6e3ae70073c0c8b8dfff09a957
jfx <j@f.x>
2019-06-21 13:04:03 +1000

    Steamguard if checks, code cleanup

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ let SteamUser = require('steam-user');
 let SteamCommunity = require('steamcommunity');
 let SteamTotp = require('steam-totp');
 let TradeOfferManager = require('steam-tradeoffer-manager');
-let SteamInventoryAPI = require('steam-inventory-api-fork');
 
 let BotManager = function(options) {
 	if (!options) options = {};
@@ -79,20 +78,22 @@ BotManager.prototype.addBot = function(loginDetails, managerEvents, type) {
 			self.bots[botIndex].steamid = client.steamID.getSteamID64();
 		});
 		
-		client.on('steamGuard', (domain, callback) => {
-			if (domain == null) {
-				console.log("Steam guard code failed, re-retrying in 30 seconds...");
-				setTimeout(function(){
-					callback(SteamTotp.getAuthCode(self.bots[botIndex].loginInfo.shared));
-				}, 30 * 1000);
-			}
-		});
+		if (loginDetails.shared) {
+			client.on('steamGuard', (domain, callback) => {
+				if (domain == null) {
+					console.log("Steam guard code failed, re-retrying in 30 seconds...");
+					setTimeout(function(){
+						callback(SteamTotp.getAuthCode(self.bots[botIndex].loginInfo.shared));
+					}, 30 * 1000);
+				}
+			});
+		}
 		
 		client.on('webSession', (sessionID, cookies) => {
 			let login = new Promise((resolve, reject) => {
 				console.log("Replacing web session");
 				community.setCookies(cookies);
-				community.startConfirmationChecker(10000, loginDetails.identity);
+				if (loginDetails.identity) community.startConfirmationChecker(10000, loginDetails.identity);
 				manager.setCookies(cookies, (err) => {
 					if (err) reject(err);
 					self.bots[botIndex].apiKey = manager.apiKey;
@@ -140,7 +141,7 @@ BotManager.prototype.retryLogin = function(botIndex) {
 	//console.log(self.bots[botArrayLength-1]);
 	let loginDetails = bot.loginInfo;
 	//console.log(self.bots);
-	loginDetails.twoFactorCode = SteamTotp.getAuthCode(loginDetails.shared);
+	if (loginDetails.shared) loginDetails.twoFactorCode = SteamTotp.getAuthCode(loginDetails.shared);
 
 	if (!bot.client.steamID) { //If we need to log it into Steam
 		console.log("Logging into Steam Client");
@@ -153,16 +154,16 @@ BotManager.prototype.retryLogin = function(botIndex) {
 
 BotManager.prototype.loadInventories = function(appid, contextid, tradableOnly) {
 	return Promise.all(this.bots.map((bot, i) => {
-    return this.inventoryApi.get({
-      appid,
-      contextid,
-      retries: 100,
-      retryDelay: 3000,
-      steamid: bot.steamid,
-      tradable: tradableOnly,
-    })
+		return this.inventoryApi.get({
+			appid,
+			contextid,
+			retries: 100,
+			retryDelay: 3000,
+			steamid: bot.steamid,
+			tradable: tradableOnly,
+		})
 		.then((res) => {
-      const inventory = res.items;
+			const inventory = res.items;
 			inventory.forEach((item) => item.botIndex = i);
 			return inventory;
 		});
@@ -172,129 +173,12 @@ BotManager.prototype.loadInventories = function(appid, contextid, tradableOnly) 
 	});
 };
 
-BotManager.prototype.addJob = function(job) {
-	if (job.length > 0) job.forEach(function(job) { this.botJobs.push(job) });
-	else this.botJobs.push(job);
-};
-
-BotManager.prototype.processJobs = function(jobsToProcess) {
-   if (!jobsToProcess) jobsToProcess = 1;
-	let jobProcesses = [];
-   for (let i = 0; i < jobsToProcess; i++) {
-	  jobProcesses.push(this.processJob(this.botJobs.shift()));
-   }
-	return jobProcesses;
-};
-
-BotManager.prototype.processJob = function({type, multi, constraints, args, fn, bots}) {
-	return new Promise((resolve, reject) => {
-		console.log('New job:\n\t-', type, bots, multi, constraints);
-
-		//Get an array of bot indexes, which are permitted to do the job
-		if (!bots) bots = this.bots.map((bot) => bot.botIndex);
-		else if (typeof bots == 'number') bots = [bots];
-		else if (!Array.isArray(bots)) throw 'options.bots is not in a valid format';
-		//Test constraints for bots permitted
-		if (constraints) {
-			bots = bots.filter((botIndex) => {
-				return constraints.reduce((prev, constraintName) => {
-					console.log('Testing',constraintName+'. The result was:', this.testConstraint(constraintName, args, botIndex));
-					return this.testConstraint(constraintName, args, botIndex) && prev;
-				}, true);
-			});
-		}
-
-		console.log('Bots (of selected) which pass all job constraints', bots);
-
-		if (bots.length < 1) return reject('No bots meet all the criteria');
-
-		let botObjects;
-		if (!multi) botObjects = this.bots[bots[0]];
-		else botObjects = bots.map((botIndex) => this.bots[botIndex]);
-		resolve(botObjects);
-	})
-	.then((botObjects) => Promise.resolve(fn(args, botObjects)))
-	.then((res) => {
-		console.log('A job of type: ' + type + ' just completed\n\t- ' + res);
-		if (bots && constraints) {
-			bots.forEach((botIndex) => {
-				constraints.forEach((constraintName) => {
-					let constraint = this.jobConstraints[constraintName];
-					if (constraint) {
-						if (constraint.succeededChange(args) !== undefined)
-							constraint.botConstraintValues[botIndex] += constraint.succeededChange(args);
-					}
-				});
-			});
-		}
-	})
-	.catch((err) => {
-		console.log('There was an error completing a job of type: ' + type + '\n\t- ' + err);
-		bots.forEach((botIndex) => {
-			constraints.forEach((constraintName) => {
-				let constraint = this.jobConstraints[constraintName];
-				if (constraint) {
-					if (constraint.failedChange(args) !== undefined)
-						constraint.botConstraintValues[botIndex] += constraint.failedChange(args);
-				}
-			});
-		});
-		throw err;
-	});
-};
-
-BotManager.prototype.addJobConstraint = function({name, initialValue, failedChange, succeededChange, testConstraint}) {
-	if (!name) throw 'options.name not set';
-	if (!testConstraint) throw 'options.testConstraint not set';
-	if (!succeededChange && !failedChange) throw 'neither options.succeededChange or options.failedChange are defined';
-	if (!initialValue) throw 'options.initialValue not set';
-
-	this.jobConstraints[name] = {
-		initialValue: initialValue,
-		failedChange: failedChange,
-		succeededChange: succeededChange,
-		testConstraint: testConstraint,
-		botConstraintValues: []
-	};
-	return 'constraint has been added';
-};
-
-BotManager.prototype.testConstraint = function(constraintName, args, botIndex) {
-	if (!this.jobConstraints[constraintName]) return;
-	const constraintTest = this.jobConstraints[constraintName].testConstraint;
-
-	let botConstraintValues = this.jobConstraints[constraintName].botConstraintValues;
-	if (botIndex && botConstraintValues[botIndex]) {
-		return constraintTest(this.bots[botIndex], botConstraintValues[botIndex], args);
-	} else if (botIndex !== undefined) {
-		botConstraintValues[botIndex] = this.jobConstraints[constraintName].initialValue(botIndex);
-		return constraintTest(this.bots[botIndex], botConstraintValues[botIndex], args);
-	} else {
-		return this.bots.map((bot, i) => {
-			if (!botConstraintValues[i]) {
-				botConstraintValues[botIndex] = this.jobConstraints[constraintName].initialValue(botIndex);
-			}
-			return constraintTest(bot, botConstraintValues[i], args) ? i : undefined;
-		}).filter((val) => val !== undefined);
-	}
-};
-
 BotManager.prototype.botIndexFromSteamid = function(steamid) {
 	return this.bots.map((bot) => bot.steamid).indexOf(steamid);
 };
 
 BotManager.prototype.botSteamidFromIndex = function(botIndex) {
 	return this.bots[botIndex].steamid;
-};
-
-BotManager.prototype.openJobs = function() {
-	return this.botJobs.length;
-};
-
-BotManager.prototype.setConstraintValues = function(name, value) {
-	if (this.jobConstraints[name]) {
-		this.jobConstraints[name].botConstraintValues = this.jobConstraints[name].botConstraintValues.map(() => value);
-   }
 };
 
 BotManager.prototype.numberOfBotsLoggedIn = function() {
